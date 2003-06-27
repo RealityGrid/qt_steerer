5ca710ffdbc12b528e5fd86d3908fa8fcd992e75
Mark Riding <mark.riding@manchester.ac.uk>
2003-06-27 15:48:01 +0000

    MR: ParameterHistory class used to keep a record of all parameter values in order to produce graphs...

---
+++ inc/parameterhistory.h
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------
+    Header file for ParameterHistory class for QT steerer GUI
+
+    (C)Copyright 2002 The University of Manchester, United Kingdom,
+    all rights reserved.
+
+    This software is produced by the Supercomputing, Visualization &
+    e-Science Group, Manchester Computing, the Victoria University of
+    Manchester as part of the RealityGrid project.
+
+    This software has been tested with care but is not guaranteed for
+    any particular purpose. Neither the copyright holder, nor the
+    University of Manchester offer any warranties or representations,
+    nor do they accept any liabilities with respect to this software.
+
+    This software must not be used for commercial gain without the
+    written permission of the authors.
+
+    This software must not be redistributed without the written
+    permission of the authors.
+
+    Permission is granted to modify this software, provided any
+    modifications are made freely available to the original authors.
+
+    Supercomputing, Visualization & e-Science Group
+    Manchester Computing
+    University of Manchester
+    Manchester M13 9PL
+
+    WWW:    http://www.sve.man.ac.uk
+    email:  sve@man.ac.uk
+    Tel:    +44 161 275 6095
+    Fax:    +44 161 275 6800
+
+    Initial version by: M Riding, 23.06.2003
+
+---------------------------------------------------------------------------*/
+
+#ifndef __PARAMETERHISTORY_H__
+#define __PARAMETERHISTORY_H__
+
+#include "qvaluelist.h"
+
+class ParameterHistory {
+  public:
+    ParameterHistory();
+    ~ParameterHistory();
+  
+    void updateParameter(const char* lVal);
+    const char* elementAt(int index);
+    void graphParameter();
+
+    int size;
+
+//  private:
+    typedef QValueList<const QString> ParamHistListType;
+    ParamHistListType mParamHistList;
+};
+
+#endif
+
