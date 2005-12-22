ff719b2eec4d4bbf00761da3a1856671f2e30604
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-12-22 12:18:55 +0000

    Class for reading, storing and writing configuration data.

---
+++ inc/steererconfig.h
@@ -0,0 +1,81 @@
+/*----------------------------------------------------------------------------
+  ControlForm class header file for QT steerer GUI.
+
+  (C) Copyright 2002, 2004, University of Manchester, United Kingdom,
+  all rights reserved.
+
+  This software is produced by the Supercomputing, Visualization and
+  e-Science Group, Manchester Computing, University of Manchester
+  as part of the RealityGrid project (http://www.realitygrid.org),
+  funded by the EPSRC under grants GR/R67699/01 and GR/R67699/02.
+
+  LICENCE TERMS
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+  THIS MATERIAL IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. THE ENTIRE RISK AS TO THE QUALITY
+  AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE
+  DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR
+  CORRECTION.
+
+  Initial version by Andrew Porter, 21.12.2005.
+
+---------------------------------------------------------------------------*/
+
+#ifndef _STEERERCONFIG_H_
+#define _STEERERCONFIG_H_
+
+using namespace std;
+
+/** @file steererconfig.h
+    @brief Defines class holding configuration data
+*/
+
+class SteererConfig {
+  public:
+
+  // Member variables
+  
+  /** Address of the top-level registry */
+  QString mTopLevelRegistry;
+  /** Path of directory holding the CA certificates */
+  QString mCACertsPath;
+  /** Location of the PEM file holding user's private key and certificate */
+  QString mPrivateKeyCertFile;
+  /** Whether automatic polling should be switched on by default */
+  bool mAutoPollingOn;
+  /** The default polling interval when not setting it automatically */
+  float mPollingIntervalSecs;
+  /** Whether or not to show the table of monitored parameters by default */
+  bool mShowMonParamTable;
+  /** Whether or not to show the table of steerable parameters by default */
+  bool mShowSteerParamTable;
+  /** Whether or not to show the table of IOTypes by default */
+  bool mShowIOTypeTable;
+  /** Whether or not to show the table of ChkTypes by default */
+  bool mShowChkTypeTable;
+
+  SteererConfig();
+  ~SteererConfig();
+
+  /** Write a configuration file for the steering client using current settings */
+  void writeConfig(QString fileName);
+  /** Read configuration file and store values in member variables of this class */
+  void readConfig(QString fileName);
+  /** Get the value (from the "value" attribute) of the named 
+      element which is to be found somewhere below the elem element */
+  QString getElementAttrValue(QDomElement elem, 
+			      QString     name);
+};
+
+#endif
---
+++ src/steererconfig.cpp
@@ -0,0 +1,303 @@
+/*----------------------------------------------------------------------------
+    Application Class for QT launcher GUI.
+    Implementation
+
+    (C)Copyright 2003 The University of Manchester, United Kingdom,
+    all rights reserved.
+
+    This software is produced by the Supercomputing, Visualization &
+    e-Science Group, Manchester Computing, the Victoria University of
+    Manchester as part of the RealityGrid project.
+
+    This software has been tested with care but is not guaranteed for
+    any particular purpose. Neither the copyright holder, nor the
+    University of Manchester offer any warranties or representations,
+    nor do they accept any liabilities with respect to this software.
+
+    This software must not be used for commercial gain without the
+    written permission of the authors.
+    
+    This software must not be redistributed without the written
+    permission of the authors.
+
+    Permission is granted to modify this software, provided any
+    modifications are made freely available to the original authors.
+ 
+    Supercomputing, Visualization & e-Science Group
+    Manchester Computing
+    University of Manchester
+    Manchester M13 9PL
+    
+    WWW:    http://www.sve.man.ac.uk  
+    email:  sve@man.ac.uk
+    Tel:    +44 161 275 6095
+    Fax:    +44 161 275 6800    
+
+    Initial version by: M Riding, 29.09.2003
+    
+---------------------------------------------------------------------------*/
+
+/** @file SteererConfig.cpp
+ *  @brief Holds configuration data for the steering client */
+
+#include <iostream>
+#include <qdom.h>
+#include <qfile.h>
+#include <qdir.h>
+#include <qstring.h>
+#include "steererconfig.h"
+
+using namespace std;
+
+/** Constructor */
+SteererConfig::SteererConfig(){
+
+}
+
+/** Destructor */
+SteererConfig::~SteererConfig(){
+
+}
+
+/** Method loads a configuration xml file, and parses it to
+ *  determine the stored configuration values.
+ */
+void SteererConfig::readConfig(QString fileName){
+  
+  QDomDocument doc( "steererConfigDocument" );
+  QFile        configFile(fileName);
+  QDomNodeList sectionNodeList;
+  QDomNode     tmpNode;
+  QString      flag;
+
+  // Parse file
+  if ( !configFile.open( IO_ReadOnly ) ){
+    cout << "Input file " << configFile.name() << " does not exist :-(" <<endl;
+    return;
+  }
+  if ( !doc.setContent( &configFile ) ) {
+    configFile.close();
+    return;
+  }
+  configFile.close();
+
+  // Root element
+  QDomElement docElem = doc.documentElement();
+
+  // Address of top-level registry
+  mTopLevelRegistry =  getElementAttrValue(docElem, "topLevelRegistry");
+  cout << "Top-level registry is " << mTopLevelRegistry << endl;
+
+  // Security section
+  QDomNodeList nodeList = docElem.elementsByTagName("Security");
+  if(nodeList.count() != 1){
+    cout << "Failed to find Security section in config. file" << endl;
+  }
+  else{
+    mCACertsPath = getElementAttrValue(nodeList.item(0).toElement(),
+				       "caCertsPath");
+    cout << "Path to dir holding CA certs. is " << mCACertsPath << endl;
+
+    mPrivateKeyCertFile = getElementAttrValue(nodeList.item(0).toElement(),
+					      "privateKeyCertFile");
+    cout << "Path to file holding user's key and cert. is " << 
+      mPrivateKeyCertFile << endl;
+  }
+
+  // Polling configuration section
+  nodeList = docElem.elementsByTagName("Polling");
+  if(nodeList.count() != 1){
+    cout << "Failed to find Polling section in config. file" << endl;
+  }
+  else{
+    flag = getElementAttrValue(nodeList.item(0).toElement(),
+			       "autoPolling");
+    mAutoPollingOn = (flag.contains("on") == 1);
+    if(mAutoPollingOn){
+      cout << "Automatic polling is ON" << endl;
+    } else {
+      cout << "Automatic polling is OFF" << endl;
+    }
+
+    flag = getElementAttrValue(nodeList.item(0).toElement(),
+			       "pollingInterval");
+    mPollingIntervalSecs = flag.toFloat();
+    cout << "Default fixed polling interval is " << mPollingIntervalSecs << endl;
+  }
+
+  // GUI display section
+  nodeList = docElem.elementsByTagName("Display");
+  if(nodeList.count() != 1){
+    cout << "Failed to find Display section in config. file" << endl;
+  }
+  else{
+    // monitored parameters table
+    flag = getElementAttrValue(nodeList.item(0).toElement(),
+			       "showMonParamTable");
+    mShowMonParamTable = (flag.contains("on") == 1);
+
+    if(mShowMonParamTable){
+      cout << "Display of monitored params. table is ON" << endl;
+    } else {
+      cout << "Display of monitored params. table is OFF" << endl;
+    }
+
+    // steered parameters table
+    flag = getElementAttrValue(nodeList.item(0).toElement(),
+			       "showSteerParamTable");
+    mShowSteerParamTable = (flag.contains("on") == 1);
+    if(mShowSteerParamTable){
+      cout << "Display of steered params. table is ON" << endl;
+    } else {
+      cout << "Display of steered params. table is OFF" << endl;
+    }
+
+    // IOTypes table
+    flag = getElementAttrValue(nodeList.item(0).toElement(),
+			       "showIOTypesTable");
+    mShowIOTypeTable = (flag.contains("on") == 1);
+    if(mShowIOTypeTable){
+      cout << "Display of IOTypes table is ON" << endl;
+    } else {
+      cout << "Display of IOTypes table is OFF" << endl;
+    }
+
+    // ChkTypes table
+    flag = getElementAttrValue(nodeList.item(0).toElement(),
+			       "showChkTypesTable");
+    mShowChkTypeTable = (flag.contains("on") == 1);
+    if(mShowChkTypeTable){
+      cout << "Display of ChkTypes table is ON" << endl;
+    } else {
+      cout << "Display of ChkTypes table is OFF" << endl;
+    }
+  }
+
+  return;
+}
+
+//----------------------------------------------------------------------
+
+QString SteererConfig::getElementAttrValue(QDomElement elem, QString name)
+{
+  QDomNode     tmpNode;
+  QDomNodeList nodeList = elem.elementsByTagName(name);
+
+  if(nodeList.count() != 1){
+    cout << "Failed to find " << name << " in config. file" << endl;
+  }
+  else{
+    tmpNode = nodeList.item(0).attributes().namedItem("value");
+    if(!tmpNode.isNull()){
+      return tmpNode.nodeValue();
+    }
+  }
+  return "";
+}
+
+/** Create an xml encoding of the current configuration
+ *  and save the file to disk
+ */
+void SteererConfig::writeConfig(QString fileName){
+
+  QDomDocument *doc = new QDomDocument();
+
+  /*
+  // Get root node
+  QDomElement root = doc->createElement("LaunchSimulation");
+  doc->appendChild(root);
+
+  // Where to launch job
+  QDomElement eMachine = doc->createElement("TargetHostname");
+  root.appendChild(eMachine);
+
+  QDomText tMachineName = doc->createTextNode(mTargetMachine->mName);
+  eMachine.appendChild(tMachineName);
+
+  // What the jobmanager is called
+  QDomElement eJobMgr = doc->createElement("TargetHostJobManager");
+  root.appendChild(eJobMgr);
+
+  QDomText tJobMgr = doc->createTextNode(mTargetMachine->mJobManager);
+  eJobMgr.appendChild(tJobMgr);
+
+  // Wall-clock time of job in minutes
+  QDomElement eRunTime = doc->createElement("RunTime");
+  root.appendChild(eRunTime);
+
+  QDomText tRunTime = doc->createTextNode(QString::number(mTimeToRun));
+  eRunTime.appendChild(tRunTime);
+
+  // Checkpoint GSH
+  QDomElement eChkGSH;
+  QDomText tChkGSH;
+  if(restart || migration){
+    eChkGSH = doc->createElement("CheckPointGSH");
+    root.appendChild(eChkGSH);
+
+    tChkGSH = doc->createTextNode(currentCheckpointGSH);
+    eChkGSH.appendChild(tChkGSH);
+  }
+
+  // No. of processors
+  QDomElement eNumProc = doc->createElement("NoProcessors");
+  root.appendChild(eNumProc);
+
+  QDomText tNumProc = doc->createTextNode(QString::number(mNumberProcessors));
+  eNumProc.appendChild(tNumProc);
+
+  // Input file(s) for simulation
+  QDomElement eInputFiles = doc->createElement("SimulationInputFiles");
+  root.appendChild(eInputFiles);
+
+  QDomElement eFile;
+  QDomText tFile;
+  if(mAppToLaunch->mHasInputFile){
+  
+    eFile =  doc->createElement("File");
+    eInputFiles.appendChild(eFile);
+
+    // Read input file
+    QFile file(mInputFileName);
+    file.open( IO_ReadOnly );
+    QByteArray fileData = file.readAll();
+    file.close();
+  
+    // Encode input file as base64
+    QCString fileDataB64 = QCodecs::base64Encode(fileData, true);
+  
+    tFile = doc->createTextNode(fileDataB64);
+    eFile.appendChild(tFile);
+  }
+
+  // Pull out GSH of SGS and use to create job ID too
+  QString outputFile("ReGJob.");
+  QDomText tSGS;
+  if(mAppToLaunch->mNumInputs == 0){
+    tSGS = doc->createTextNode(simulationGSH);
+    outputFile.append(simulationGSH.right(simulationGSH.length() - simulationGSH.findRev('/') - 1));
+  }
+  else{
+    tSGS = doc->createTextNode(visualizationGSH);
+    outputFile.append(visualizationGSH.right(visualizationGSH.length() - visualizationGSH.findRev('/') - 1));
+  }
+
+  // Name of file to write job stdout to
+  QDomElement eStdOut = doc->createElement("SimulationSTDOUTfile");
+  root.appendChild(eStdOut);
+  QDomText tStdOut = doc->createTextNode(outputFile+".stdout");
+  eStdOut.appendChild(tStdOut);
+
+  // Name of file to write job stderr to
+  QDomElement eStdErr = doc->createElement("SimulationSTDERRfile");
+  root.appendChild(eStdErr);
+  QDomText tStdErr = doc->createTextNode(outputFile+".stderr");
+  eStdErr.appendChild(tStdErr);
+
+  // GSH of associated SGS
+  QDomElement eSGS = doc->createElement("ReGSGSAddress");
+  root.appendChild(eSGS);
+  eSGS.appendChild(tSGS);
+  */
+  return;
+}
