ff1b0b508a509054ece425334552e91c52174838
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-01-26 14:05:00 +0000

    tidied comments and move to Doxygen form

--- inc/types.h
+++ inc/types.h
@@ -40,25 +40,23 @@
 #define REG_STR_CPP 1
 #include "ReG_Steer_types.h"
 
-#define kNULL				  0
+#define kNULL                   0
 
-//SMR XXX move these to table class files??
-
-// parameter table constants
+/// parameter table constants
 #define kID_COLUMN		0
 #define kNAME_COLUMN		1
 #define kREG_COLUMN		2
 #define kVALUE_COLUMN		3
 #define kNEWVALUE_COLUMN	4
 
-//parameter tables sizes
+/// parameter tables sizes
 #define kNUM_MON_COLUMNS	4
 #define kNUM_STEER_COLUMNS	5 
 // MR: removed - was causing problems, and giving no noticeable benefit
 //#define kINIT_ROWS		0//3
 
 
-//iotype table constants
+/// iotype table constants
 #define kIO_ID_COLUMN		0
 #define kIO_NAME_COLUMN		1
 #define kIO_VALUE_COLUMN	2
@@ -67,30 +65,33 @@
 #define kIO_RESTART_COLUMN	5  // chkpoint only
 
 
-//iotype tables sizes
+/// iotype tables sizes
 #define kNUM_IO_COLUMNS		5  // not include kIO_RESTART_COLUMN
 // MR: removed - was causing problems, and giving no noticeable benefit
 //#define kIO_INIT_ROWS		0//2
 
-// iotype new frequency not set indicator
+/// iotype new frequency not set indicator
 #define kNULL_FREQ		-1
 
-// iotype checkpoint null restart index
+/// iotype checkpoint null restart index
 #define kNULL_INDX		-1
 
-// checkpoint command parameter string length
+/// checkpoint command parameter string length
 #define kCHKPT_PARAM_LEN	REG_MAX_STRING_LENGTH + 5
 
 
-// minimum polling interval (milliseconds)from commsthread Get_next_message
+/// minimum polling interval (milliseconds) from commsthread 
+/// Get_next_message
 #define kMIN_POLLING_INT	1
 #define kMAX_POLLING_INT        1000
 
-// Unique numbers to make QCustomEvent IDs for postEvent from  CommsThread.cpp
+/// Unique numbers to make QCustomEvent IDs for postEvent 
+/// from CommsThread.cpp
 #define kMSG_EVENT		100
 #define kSIGNAL_EVENT		200
  
-
+/// Maximum number of plots in a single history plot
+#define kMAX_HISTORY_PLOTS      10
 
 #endif
 
