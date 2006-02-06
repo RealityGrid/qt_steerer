d5470fbadb069955e6155d943abe5be035539823
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-02-06 12:25:06 +0000

    Remove plotcount parameter

--- inc/parameter.h
+++ inc/parameter.h
@@ -88,9 +88,6 @@ public:
   QString getLabel();
 
   ParameterHistory  *mParamHist;
-  /// Keeps a count of how many plots this parameter is
-  /// an ordinate for
-  int                mPlotCount;
 
 private:
   /// Whether or not this parameter is steerable
